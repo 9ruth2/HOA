2f7b6cbdc905cb02c239c2bedb9246baefe96072
unknown <9ruth2@gmail.com>
2019-06-03 12:27:41 +0300

    upload firebase

--- src/App.js
+++ src/App.js
@@ -1,12 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+//import logo from './logo.svg';
 import './App.css';
-import Dummy from './Dummy'
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth'
 
-function App() {
+class App extends Component {
+
+  state = {
+    name: null
+  }
+
+  render() {
+  // const aut = firebase.auth()
+  // aut.createUserWithEmailAndPassword("hello@gmail.com", "123456789")
+  // aut.currentUser.email
+  // aut.signOut()
+
+
+  const fs = firebase.firestore()
+  fs.collection("Apt").doc("74FxCUzaLcOXH3Lcqyxh").get().then(result => {
+    this.setState({
+      name: result.data().name
+    })
+    
+  })
+  // fs.collection('Apt').add({
+  //   name: 'hello',
+  //   age: 555
+  // })
+  if(this.state.name == null) {
+    return <h1>Loading...</h1>
+  }
   return (
-    <Dummy color1='red' color2='green' fff={123}/>
+    <h1>{this.state.name}</h1>
   );
+  }
 
 }
 
