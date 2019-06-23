a4fc1ca77d208cf00e7ddf3aff3c328af9d6026b
Yarden <yarden.paniry@gmail.com>
2019-06-23 14:24:18 +0300

    design+calendar

--- src/components/navBar/NavBar.js
+++ src/components/navBar/NavBar.js
@@ -6,20 +6,19 @@ class NavBar extends Component {
 
     render() {
         return(
-        <React.Fragment>
-            <ul>
-            <li><a class="active" href="message">לוח הודעות</a></li>
-            <li><a href="calendar">לוח אירועים</a></li>
-            <li><a href="/tenant">פרטים אישיים</a></li>
-            <li><a href="payment/payment-main-page">תשלומים ודוחות</a></li>
-            <li><a href="/user-page">ניהול משתמשים</a></li>
-            <li><a href="create-building">ניהול בניינים</a></li>
-            <li style={{float:'right'}}><a ><LogOut/></a></li>
+            <ul className="NavBar_ul">
+
+            <li className="NavBar_li"><a className="NavBar_li_a" href="message">לוח הודעות</a></li>
+            <li className="NavBar_li"><a className="NavBar_li_a" href="calendar">לוח אירועים</a></li>
+            <li className="NavBar_li"><a className="NavBar_li_a" href="/tenant">פרטים אישיים</a></li>
+            <li className="NavBar_li"><a className="NavBar_li_a" href="payment/payment-main-page">תשלומים ודוחות</a></li>
+            <li className="NavBar_li"><a className="NavBar_li_a" href="/user-page">ניהול משתמשים</a></li>
+            <li className="NavBar_li"><a className="NavBar_li_a" href="/createBuilding">ניהול בניינים</a></li>
+
+            <li className="NavBar_logout" style={{float:'left'}}><a className="NavBar_li_a" ><LogOut/></a></li>
             </ul>
-        </React.Fragment>
         )
     }
-
 
 }
 
