b614a8362c13ae7c1803a196ef48937c46611a5d
shakedco <shaked.cohen1993@gmail.com>
2019-06-28 17:01:07 +0300

    export to excel - payment table

--- src/components/Payment/PaymentTable.js
+++ src/components/Payment/PaymentTable.js
@@ -5,7 +5,29 @@ import 'firebase/auth'
 import './PaymentTable.css';
 import NavBar from '../navBar/NavBar';
 import { Link } from 'react-router-dom'
+import { Button } from '@material-ui/core/';
+import XLSX from 'xlsx';
 
+// a func to convert object to arr
+const newPaymentToArr = newPayment => [{key: "amount"},{key:"details"}].map(r => newPayment[r.key]);
+
+const set_right_to_left = wb => {
+    if (!wb.Workbook) wb.Workbook = {};
+    if (!wb.Workbook.Views) wb.Workbook.Views = [];
+    if (!wb.Workbook.Views[0]) wb.Workbook.Views[0] = {};
+    wb.Workbook.Views[0].RTL = true;
+  };
+            
+// a func to convert aoa to file //
+const aoaToFile = ({ fileName, sheetName = 'Sheet1', aoa }) => {
+    if (aoa) {
+        const workbook = XLSX.utils.book_new();
+        set_right_to_left(workbook);
+        const sheet = XLSX.utils.aoa_to_sheet(aoa);
+        XLSX.utils.book_append_sheet(workbook, sheet, sheetName);
+        XLSX.writeFile(workbook, fileName + '.xlsx');
+    }
+};
 
 class PaymentTable extends Component{
    
@@ -23,11 +45,38 @@ class PaymentTable extends Component{
         }
     }
 
+     // a func to export to excel
+	exportToExcel = () => {
+        const columnNames = ["סכום התשלום הכולל","פירוט התשלום"];
+        const aoa = [columnNames].concat(this.state.tableData.map(this.newPaymentToArr));
+        aoaToFile({ fileName: 'payment report.xlsx', aoa });
+    }
+            
+    // a func to convert object to arr
+    newPaymentToArr = newPayment => [{key: "amount"},{key:"details"}].map(r => newPayment[r.key]);
+    
+            
+    // a func to convert aoa to file //
+        aoaToFile = ({ fileName, sheetName = 'Sheet1', aoa }) => {
+        if (aoa) {
+            const workbook = XLSX.utils.book_new();
+            set_right_to_left(workbook);
+            const sheet = XLSX.utils.aoa_to_sheet(aoa);
+            XLSX.utils.book_append_sheet(workbook, sheet, sheetName);
+            XLSX.writeFile(workbook, fileName + '.xlsx');
+        }
+    };
 
     render() { 
         return(
             <div>
                 <NavBar/>
+                <Button
+                    size="large"
+                    variant="contained"
+                    color="primary"
+                    onClick={this.exportToExcel}>
+                </Button>        
                 <h1 className="bigTitle">דוח תשלום ועד הבניין</h1>
                 <table id = "paymentTable" border= "1" style = {this.PaymentTableStyle()}>
                     
@@ -86,6 +135,7 @@ class PaymentTable extends Component{
         })
     }
 
+   
 
 }
 
