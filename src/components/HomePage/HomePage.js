77e22fe58eca996da7478a210a591aa5f76e3375
Noy Levy <noylevy12@gmail.com>
2019-06-28 17:21:07 +0300

    style

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -9,17 +9,33 @@ class HomePage extends Component {
 
     render() {
         return(      
+            
             <div className = "HomePage">
             <NavBar/>
-                <p>
-                    <h1 align="center" className= "aboutAsTitle">ניהול נכון חיים נכון</h1>
-                    <h4 align="center" className= "aboutAsTitle">שיפור אורח החיים שלך ושל שכנייך מתחיל כאן</h4>
-
+            <div id="home" className = "fadeIn">
+                <p className = "title">
+                    <h2 className="bigTitle">ברוכים הבאים למערכת לניהול ועד הבית</h2>
+                    <img className="imgTitle" src="http://www.up2me.co.il/images/61403197.png"></img>
                 </p>
-                <div align="right" className="MessageArea">
-                    <Message/>
+            </div>
+            <div id="about" className= "slideLeft">
+                <div className = "zoneB">
+                <div className="slideLeft">
+                    <div align="right" className="MessageArea">
+                        <Message/>
+                    </div>
+                </div>
+                <div className = "aboutAs">
+                    <h2 className="bigTitle">?מי אנחנו</h2>
+                    <p>פרויקט קהילה בונה מבית הקרן לירושלים ומנהל קהילתי יובלים הוא מודל עבודה לשיפור איכות החיים באמצעות קידום שיפוצים פיזיים בדירות, בבניינים ובחצרות, תוך קיום מפגשי תרבות, תוכן, קיימות ומנהיגות לקידום קשרים בקהילה, שכנות טובה ותרבות דיור. 
+    אתר זה נועד לסייע לכם בניהול התחזוקה של הבניין וכפלטפורמה לתקשורת בין הדיירים.
+                    </p>
+                </div>
                 </div>
             </div>
+
+            </div>
+            
         )
     }
 }
