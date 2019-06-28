b9787528fea4eae3b09016576bf45347487fe594
unknown <9ruth2@gmail.com>
2019-06-28 18:46:10 +0300

    fg

--- src/components/Payment/PaymentMainPage.js
+++ src/components/Payment/PaymentMainPage.js
@@ -9,21 +9,36 @@ import '../Style/Style.css';
 
 class PaymentMainPage extends Component{
 
+    state=({
+        classStyle:''
+    })
+
     PaymentMainPagetStyle = () => {
         return{
             textAlign: 'center',
             paddingRight: '1em'  
         }
     }
+    hoaSeeOnly = () => {
+        const user = firebase.auth().currentUser.uid;
+        firebase.firestore().collection('Apt').doc(user).get().then(result=>{
+            if(result.data().hoa === true){
+                this.setState({classStyle:'hoaSeeOnly'})
+            }
+        })
+    }
     
     render(){
+        this.hoaSeeOnly()
+
+
+
         return(
             <div className = "PaymentMainPage" style = {this.PaymentMainPagetStyle()} >
                 <NavBar/>
                 <h1 className = "mainTitle">תשלומים</h1>
                 <h3 className = "smallTitle"> עמוד ראשי</h3>
-
-                <button className="buttonStyle"><Link to="/payment/create-payment" className="LinkStyle">הוספת תשלום</Link></button>
+                <button className={this.state.classStyle} className="buttonStyle"><Link to="/payment/create-payment" className="LinkStyle">הוספת תשלום</Link></button>
                 <p></p>
                 <button className="buttonStyle"><Link to="/payment/payment-table"className="LinkStyle">דוח תשלומים</Link></button>
 
